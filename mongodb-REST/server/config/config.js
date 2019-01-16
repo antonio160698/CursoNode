9883d726860f56805d09cedfc07e7fc75ffa7ab8
Giovanny <antonio.elizondolo@uanl.edu.mx>
2019-01-16 16:01:50 -0600

    Se ha creado un crud completo utilizando mongo y se utilizo mLab para que la base de datos este en la nube

--- mongodb-REST/server/config/config.js
+++ mongodb-REST/server/config/config.js
@@ -1,3 +1,15 @@
 //Puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+//entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//base de datos
+let urlDB;
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb://cafeUser:hola1234@ds159184.mlab.com:59184/cafe';
+// }
+process.env.URLDB = urlDB;
